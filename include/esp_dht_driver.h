a7ac22daada7ef99939406df88d71fea42fce2cc
otaxhu <oscarperniamoreno@gmail.com>
2024-03-04 13:37:28 -0400

    feat: documentation of API
    fix: changed dht_delete signature

--- include/esp_dht_driver.h
+++ include/esp_dht_driver.h
@@ -15,14 +15,6 @@ extern "C" {
 typedef void *dht_handle_t;
 
 /**
- * Type of dht
-*/
-typedef enum {
-    DHT_TYPE_11,
-    DHT_TYPE_22
-} dht_type_t;
-
-/**
  * Config type to be passed to dht_new() function
  * 
  * @note Config gets copied in dht_new() function so you don't have to worry about it being on the
@@ -30,7 +22,6 @@ typedef enum {
 */
 typedef struct dht_config {
     gpio_num_t gpio_pin;
-    dht_type_t type;
 } dht_config_t;
 
 /**
@@ -48,12 +39,38 @@ typedef struct dht_data {
 } dht_data_t;
 
 /**
- * Creates a new instance of dht sensor, 
+ * Creates a new instance of dht sensor.
+ * 
+ * @param config Pointer to structure of configuration, must not be NULL
+ * @param handle Pointer to handle type, must not be NULL
+ * 
+ * @returns
+ *  - ESP_OK on success.
+ *  - ESP_ERR_INVALID_ARG if the parameters are NULL.
+ *  - ESP_ERR_NO_MEM if there was no memory to allocate "handle" data structure
 */
 esp_err_t dht_new(dht_config_t *config, dht_handle_t *handle);
 
-esp_err_t dht_delete(dht_handle_t handle);
+/**
+ * Frees up dht_handle_t created by dht_new() function.
+ * 
+ * @param handle Handle created by successful call to dht_new() function
+ * 
+ * @note Is undefined behaviour if "handle" was already freed up by this function
+*/
+void dht_delete(dht_handle_t handle);
 
+/**
+ * Reads humidity and temperature from dht sensor.
+ * 
+ * @param handle Handle to dht sensor, created by successful call to dht_new() function
+ * @param[out] out_data Data structure to write with temperature and humidity data
+ * 
+ * @returns
+ *  - ESP_OK on success
+ *  - ESP_ERR_INVALID_ARG if the arguments are NULL
+ *  - ESP_FAIL if ocurred an error reading dht sensor (data could be written to "out_data")
+*/
 esp_err_t dht_read(dht_handle_t handle, dht_data_t *out_data);
 
 #ifdef __cplusplus
